980df19d730dc1165364206484cd8a43e99706b1
qqniqq <st50@WS50>
2025-09-18 09:47:04 +0300

    вывод значений массива из 10000 элементов

--- in.mdk.01.01/in.mdk.01.01/Program.cs
+++ in.mdk.01.01/in.mdk.01.01/Program.cs
@@ -13,7 +13,13 @@ namespace @in.mdk._01._01
             string[] array = new string[3] { "йцу", "енг", "мит" };
             array[1] = "кпрцкр";
             Console.WriteLine($"Элементы массива:{array[0]},{array[1]},{array[2]}");
-            string[] array2 = new string[2];
+            string[] array2 = new string[3];
+            int[] array3 = new int[10000];
+            for (int inde = 0; inde < array3.Length; inde++)
+            {
+                array3[inde] = inde+1;
+                Console.Write($"[{array3[inde]},]");
+            }
         }
     }
 }
