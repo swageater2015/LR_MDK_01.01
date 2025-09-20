816a12af680dc0e9ad04b34f8da4755f9272a296
st56 <alemehov1@gmail.com>
2025-09-20 12:16:43 +0300

    создание текстового списка

---
+++ LISTT/LISTT/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LISTT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list1 = new List<string>();
+        }
+    }
+}
