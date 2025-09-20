09c7d2e9e3863b95c63e9a1147ecade66124a352
st56 <alemehov1@gmail.com>
2025-09-20 13:04:17 +0300

    u

--- LISTT/LISTT/Program.cs
+++ LISTT/LISTT/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.XPath;
 
 namespace LISTT
 {
@@ -11,6 +12,23 @@ namespace LISTT
         static void Main(string[] args)
         {
             List<string> list1 = new List<string>();
+            list1.Add("eee");
+            list1.Add("zzz");
+            list1.Add("xxx");
+            list1.Add("ccc");
+            list1.Add("vvv");
+            Console.WriteLine("Введите текст:");
+            string i = Convert.ToString(Console.ReadLine());
+            Console.WriteLine(i);
+            List<string> list2 = new List<string>();
+            list1.Add($"{i}");
+            foreach (string i in list1) ;
+            if (list1.Contains(list1[0]))
+            {
+
+            }
+
+
         }
     }
 }
