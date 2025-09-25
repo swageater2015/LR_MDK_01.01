4038746824f8ac879fc96f5a5652de845031f805
swageater2015 <alemehov1@gmail.com>
2025-09-25 18:54:40 +0300

    создание и заполнение массива

--- LR 1(№1,variant 2)/LR 1(№1,variant 2)/Program.cs
+++ LR 1(№1,variant 2)/LR 1(№1,variant 2)/Program.cs
@@ -13,6 +13,16 @@ namespace LR_1__1_variant_2_
             Console.WriteLine("Введите размер массива:");
             string input = Console.ReadLine();
             int n = Convert.ToInt32(input);
+
+
+            int[] numbers = new int[n];
+            Console.WriteLine($"Введите {n} чисел:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Число {i + 1}: ");
+                string numInput = Console.ReadLine();
+                numbers[i] = Convert.ToInt32(numInput);
+            }
         }
     }
 }
