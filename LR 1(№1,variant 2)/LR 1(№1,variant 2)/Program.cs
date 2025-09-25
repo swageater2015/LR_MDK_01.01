32bbb72872878fb5c98ecdc9e756a3a66e61de9f
swageater2015 <alemehov1@gmail.com>
2025-09-25 18:56:47 +0300

    вычисление суммы всех элементов

--- LR 1(№1,variant 2)/LR 1(№1,variant 2)/Program.cs
+++ LR 1(№1,variant 2)/LR 1(№1,variant 2)/Program.cs
@@ -23,6 +23,12 @@ namespace LR_1__1_variant_2_
                 string numInput = Console.ReadLine();
                 numbers[i] = Convert.ToInt32(numInput);
             }
+
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                sum = sum + numbers[i];
+            }
         }
     }
 }
