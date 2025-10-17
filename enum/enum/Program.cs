6af524f83da2af33e4f034b8b934f8cd985734f7
swageater2015 <alemehov1@gmail.com>
2025-10-17 23:46:45 +0300

    z

---
+++ enum/enum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @enum
+{
+    internal class Program
+    {
+        enum Color
+        {
+            Красный,
+            Зеленый,
+            Синий,
+            Жёлтый
+        }
+        static void Main()
+        {
+            Console.WriteLine("Все цвета:");
+
+            foreach (Color color in Enum.GetValues(typeof(Color)))
+            {
+                Console.WriteLine(color);
+            }
+        }
+        
+    }   
+}
