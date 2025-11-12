f66338d32601a8d7eff1e98424cf1b7ad6d0dcd9
swageater2015 <alemehov1@gmail.com>
2025-11-12 23:21:43 +0300

    создание массива

---
+++ distant/students/students/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace students
+{
+    struct Student
+    {
+        public string Name;
+        public int Age;
+        public double Grade;
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] students = new Student[100];
+            Random rand = new Random();
+        }
+    }
+}
