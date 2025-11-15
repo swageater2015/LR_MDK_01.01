b98c8121bfc77570e43573941fd59c66ade8253c
Daniil5903 <danilka7518@gmail.com>
2025-11-15 12:38:43 +0300

    создание списка игр

---
+++ SalePhone/SalepPhone/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Sale
+{
+    public DateTime Date;      
+    public string PhoneName;   
+    public int Quantity;      
+    public decimal Price;   
+
+    public Sale(DateTime date, string phoneName, int quantity, decimal price)
+    {
+        Date = date;
+        PhoneName = phoneName;
+        Quantity = quantity;
+        Price = price;
+    }
+}
