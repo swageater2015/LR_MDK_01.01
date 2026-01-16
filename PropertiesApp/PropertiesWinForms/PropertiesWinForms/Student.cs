232e79b3271daba88d14d7ed353510528a010479
ponchik982 <aklimenkov34@gmail/com>
2026-01-16 09:53:00 +0300

    добавил в репозиторий

---
+++ PropertiesApp/PropertiesWinForms/PropertiesWinForms/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace PropertiesWinForms
+{
+    public class Student
+    {
+        private string name_;
+        private DateTime birthDay_;
+        private string number_;
+        private double avgBall_;
+
+        [DisplayName("ФИО")]
+        public string Name
+        {
+            get { return name_; }
+            set { name_ = value; }
+        }
+
+        [DisplayName("Дата рождения")]
+        public string BirthDay
+        {
+            get { return birthDay_.ToString("dd.mm.yyyy"); }
+        }
+        public void SetBirthDay(DateTime d)
+        {
+            birthDay_ = d;
+        }
+    }
+}
