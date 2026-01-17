dfb9a7379c99140cb473a5b22cc8dab6a1f211d0
st56 <skadeshopp@gmail.com>
2026-01-17 11:10:27 +0300

    вывод картинок

---
+++ WindowsFormsApp1/WindowsFormsApp1/MainForm.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.FileNamesListBox = new System.Windows.Forms.ListBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.PictureArea = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.EditButton = new System.Windows.Forms.Button();
+            this.LoadButton = new System.Windows.Forms.Button();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureArea)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.FileNamesListBox);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(274, 607);
+            this.panel1.TabIndex = 0;
+            // 
+            // FileNamesListBox
+            // 
+            this.FileNamesListBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.FileNamesListBox.FormattingEnabled = true;
+            this.FileNamesListBox.Location = new System.Drawing.Point(0, 0);
+            this.FileNamesListBox.Name = "FileNamesListBox";
+            this.FileNamesListBox.Size = new System.Drawing.Size(274, 607);
+            this.FileNamesListBox.TabIndex = 0;
+            this.FileNamesListBox.SelectedIndexChanged += new System.EventHandler(this.FileNamesListBox_SelectedIndexChanged);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.PictureArea);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(274, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(581, 607);
+            this.panel2.TabIndex = 1;
+            // 
+            // PictureArea
+            // 
+            this.PictureArea.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PictureArea.Location = new System.Drawing.Point(0, 0);
+            this.PictureArea.Name = "PictureArea";
+            this.PictureArea.Size = new System.Drawing.Size(581, 607);
+            this.PictureArea.TabIndex = 0;
+            this.PictureArea.TabStop = false;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.DeleteButton);
+            this.panel3.Controls.Add(this.EditButton);
+            this.panel3.Controls.Add(this.LoadButton);
+            this.panel3.Controls.Add(this.SaveButton);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(274, 508);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(581, 99);
+            this.panel3.TabIndex = 2;
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Location = new System.Drawing.Point(467, 51);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(83, 36);
+            this.DeleteButton.TabIndex = 3;
+            this.DeleteButton.Text = "Удалить";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            this.DeleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
+            // 
+            // EditButton
+            // 
+            this.EditButton.Location = new System.Drawing.Point(312, 51);
+            this.EditButton.Name = "EditButton";
+            this.EditButton.Size = new System.Drawing.Size(104, 36);
+            this.EditButton.TabIndex = 2;
+            this.EditButton.Text = "Редактировать";
+            this.EditButton.UseVisualStyleBackColor = true;
+            // 
+            // LoadButton
+            // 
+            this.LoadButton.Location = new System.Drawing.Point(173, 51);
+            this.LoadButton.Name = "LoadButton";
+            this.LoadButton.Size = new System.Drawing.Size(90, 36);
+            this.LoadButton.TabIndex = 1;
+            this.LoadButton.Text = "Загрузить";
+            this.LoadButton.UseVisualStyleBackColor = true;
+            this.LoadButton.Click += new System.EventHandler(this.LoadButton_Click);
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(25, 51);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(97, 36);
+            this.SaveButton.TabIndex = 0;
+            this.SaveButton.Text = "Сохранить\r\n";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(855, 607);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.PictureArea)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.ListBox FileNamesListBox;
+        private System.Windows.Forms.PictureBox PictureArea;
+        private System.Windows.Forms.Button DeleteButton;
+        private System.Windows.Forms.Button EditButton;
+        private System.Windows.Forms.Button LoadButton;
+        private System.Windows.Forms.Button SaveButton;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/MainForm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class MainForm: Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            FileNamesListBox.Items.Add("дверь");
+            FileNamesListBox.Items.Add("стол");
+
+        }
+
+
+        private void FileNamesListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            PictureArea.SizeMode = PictureBoxSizeMode.StretchImage;
+            PictureArea.Image = Image.FromFile(FileNamesListBox.SelectedItem.ToString());
+        }
+
+        private void LoadButton_Click(object sender, EventArgs e)
+        {
+
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+
+                openFileDialog.Filter = "jpg files (*.jpg)|*.jpg|png files (*.png)|*.png|bmp files (*.bmp)|*.bmp|All files (*.*)|*.*";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+
+                    filePath = openFileDialog.FileName;
+                    FileNamesListBox.Items.Add(filePath);
+                }
+            }
+
+
+        }
+
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+         
+            
+        }
+    }
+}
